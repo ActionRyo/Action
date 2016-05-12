03ea3825d7cc8512d3ff7aea1a27319d5df96b04
fishedee <306766045@qq.com>
2016-05-12 09:09:59 +0800

    add mysql.sql

---
+++ Mysql.sql
@@ -0,0 +1,18 @@
+create database ryo;
+
+	use ryo;
+
+	create table UserInfo{
+
+		UserID int primary key AUTO_INCREMENT,
+		UserName varchar(16) not null,
+		UserPwd varchar(16) not null,
+
+	}
+
+	create table BookInfo{
+		TableID int primary key AUTO_INCREMENT,
+		BookCode varchar(32) not null,
+		BookName varchar(32),
+		UserID int
+	}
